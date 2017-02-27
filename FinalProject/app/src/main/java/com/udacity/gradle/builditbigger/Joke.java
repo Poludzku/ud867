0bb1591b9fc61e9a8a954b7124deda9179d45eb0
Jacek Kulinski <Jacek@Jaceks-MBP.home>
2017-02-27 21:15:45 +0000

    removed parcelable, adding author text, changing endpoint to cloud

---
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/Joke.java
@@ -0,0 +1,16 @@
+package com.udacity.gradle.builditbigger;
+
+public class Joke {
+
+    private String data;
+    private String author;
+
+    public String getData() {
+        return data;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+}
--- FinalProject/jokeactivity/src/main/java/com/example/jokeactivity/JokeActivity.java
+++ FinalProject/jokeactivity/src/main/java/com/example/jokeactivity/JokeActivity.java
@@ -7,17 +7,19 @@ import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
 
-    public static String JOKE_EXTRA = "joke_extra";
+    public static final String JOKE_EXTRA = "joke_extra";
+    public static final String AUTHOR_EXTRA = "author_extra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
-
         String joke = getIntent().getStringExtra(JOKE_EXTRA);
-        if(!TextUtils.isEmpty(joke)){
+        String author = getIntent().getStringExtra(AUTHOR_EXTRA);
+        if(!TextUtils.isEmpty(joke) && !TextUtils.isEmpty(author)){
             ((TextView) findViewById(R.id.joke_text)).setText(joke);
+            ((TextView) findViewById(R.id.author_text)).setText(author);
         }
 
     }
